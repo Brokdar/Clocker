5dce99bd9685de641253fe746764ca19553cee6d
Jürgen Altszeimer <juergenaltszeimer@t-online.de>
2022-01-19 11:59:49 +0100

    display single day in table format

--- clocker/viewer.py
+++ clocker/viewer.py
@@ -1,12 +1,38 @@
 """This module is responsible for a visual representation of the model data"""
 
-from datetime import datetime
+from datetime import date, datetime
 
 from rich.console import Console
 from rich.table import Table
 
 from clocker import database
 
+
+def display_day(day: date = datetime.now().date()):
+    """Displays a specific workday.
+
+    Args:
+        day (date, optional): Date of day to be displayed. Defaults to datetime.now().date().
+    """
+
+    table = Table(title=f'Working Day - {day.strftime("%a %d.%m.%Y")}')
+    table.add_column('Start')
+    table.add_column('End')
+    table.add_column('Pause')
+    table.add_column('Duration')
+
+    console = Console()
+    workday = database.load(day)
+    if workday:
+        table.add_row(
+            workday.start.strftime("%H:%M:%S"),
+            workday.end.strftime("%H:%M:%S"),
+            str(workday.pause),
+            str(workday.duration)
+        )
+        console.print(table)
+    else:
+        console.print(f'No workday found for day: {day}')
 
 def display():
     """Displays all weekday records of the current month."""
